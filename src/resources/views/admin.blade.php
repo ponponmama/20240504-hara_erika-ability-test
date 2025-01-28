017d7ca174e652012bab5293af785d588abceb9f
ponponmama <buti1024@gmail.com>
2025-01-28 21:48:42 +0900

    adminページクラス名統合＆css修正

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -13,124 +13,116 @@
     @endsection
 
     @section('content')
-    <div class="admin-form__heading">
-        <h2 class="page-logo">
-            Admin
-        </h2>
-    </div>
+    <h2 class="page-logo">
+        Admin
+    </h2>
     <div class="page-section">
         <form class="page-main-form" action="{{ route('admin.search') }}" method="get">
             @csrf
             @php
                 $searchConditions = session('search_conditions');
             @endphp
-            <div class="search-form-container">
-                <div class="keyword-search-section">
-                    <input class="keyword-search" type="text" name="keyword"  value="{{ $searchConditions['keyword'] ?? '' }}" placeholder="名前やメールアドレスを入力してください"/>
-                </div>
-                <div class="arrow-gender-section">
-                    <select name="gender-select">
-                        <option value="">
-                            性別
-                        </option>
-                        <option value="1" {{ ($searchConditions['gender'] ?? null) == 1 ? 'selected' : '' }}>
-                            男性
-                        </option>
-                        <option value="2" {{ ($searchConditions['gender'] ?? null) == 2 ? 'selected' : '' }}>
-                            女性
-                        </option>
-                        <option value="3" {{ ($searchConditions['gender'] ?? null) == 3 ? 'selected' : '' }}>
-                            その他
-                        </option>
-                    </select>
-                    <div class="arrow__down"></div>
-                </div>
-                <div class="arrow-enquiry-section">
-                    <select class="category-select" name="category_id">
-                        <option value="" placeholder="お問い合わせの種類">
-                            お問い合わせの種類
-                        </option>
-                        @foreach($categories as $category)
-                            <option value="{{ $category->id }}" @if(($searchConditions['category_id'] ?? null) == $category->id) selected @endif>
-                                {{ $category->content }}
-                            </option>
-                        @endforeach
-                    </select>
-                    <div class="arrow__down"></div>
-                    </div>
-                    <input class="input__date" name="date" type="date" value="{{ $searchConditions['date'] ?? ' ' }}" placeholder="年/月/日" />
-                    <div class="arrow__down"></div>
-                        <button class="search__button" type="submit">
-                            検索
-                        </button>
-                        <a href="{{ route('admin.reset_search') }}" class="reset__button">リセット</a>
-                    </div>
-                    <div class="middle__admin">
-                        <div class="button__export">
-                            <a href="{{ route('admin.contacts.export_csv') }}" class="contact__export">
-                                エクスポート
-                            </a>
-                        </div>
-                        <div class="admin__pagination">
-                            {{$contacts->links()}}
-                        </div>
-                    </div>
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    @if(session('redirect_success'))
-                    <meta http-equiv="refresh" content="3;url={{ route('admin.index') }}">
-                        @php
-                            session()->forget('redirect_success');
-                        @endphp
-                    @endif
-                    <div class="listing__hail">
-                        <table id="contact__table">
-                            <tr>
-                                <th>お名前</th>
-                                <th>性別</th>
-                                <th>メールアドレス</th>
-                                <th>お問い合わせの種類</th>
-                                <th>お問い合わせの内容</th>
-                                <th></th>
-                            </tr>
-                            @foreach($contacts as $contact)
-                            <tr>
-                                <td style="display:none;">
-                                    {{ $contact->id }}
-                                </td>
-                                <td>
-                                    {{ $contact->first_name }} {{ $contact->last_name }}
-                                </td>
-                                <td>
-                                    @if ($contact->gender == 1)
-                                        男性
-                                    @elseif ($contact->gender == 2)
-                                        女性
-                                    @else
-                                        その他
-                                    @endif
-                                </td>
-                                <td>
-                                    {{ $contact->email }}
-                                </td>
-                                <td>
-                                    {{ $contact->category->content }}
-                                </td>
-                                <td>
-                                    <a href="#modal-{{ $contact->id }}" class="modal_admin"{{ $contact->id }}>
-                                        詳細
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
+            <input class="keyword-search" type="text" name="keyword"  value="{{ $searchConditions['keyword'] ?? '' }}" placeholder="名前やメールアドレスを入力してください"/>
+            <div class="arrow-gender-section">
+                <select class="gender-select" name="gender-select">
+                    <option value="">
+                        性別
+                    </option>
+                    <option value="1" {{ ($searchConditions['gender'] ?? null) == 1 ? 'selected' : '' }}>
+                        男性
+                    </option>
+                    <option value="2" {{ ($searchConditions['gender'] ?? null) == 2 ? 'selected' : '' }}>
+                        女性
+                    </option>
+                    <option value="3" {{ ($searchConditions['gender'] ?? null) == 3 ? 'selected' : '' }}>
+                        その他
+                    </option>
+                </select>
             </div>
+            <div class="arrow-enquiry-section">
+                <select class="category-select" name="category_id">
+                    <option value="" placeholder="お問い合わせの種類">
+                        お問い合わせの種類
+                    </option>
+                    @foreach($categories as $category)
+                        <option value="{{ $category->id }}" @if(($searchConditions['category_id'] ?? null) == $category->id) selected @endif>
+                            {{ $category->content }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <input class="input__date" name="date" type="date" value="{{ $searchConditions['date'] ?? ' ' }}" placeholder="年/月/日" />
+            <button class="search__button" type="submit">
+                検索
+            </button>
+            <a href="{{ route('admin.reset_search') }}" class="reset__button">
+                リセット
+            </a>
         </form>
+        <div class="middle__admin">
+            <div class="button__export">
+                <a href="{{ route('admin.contacts.export_csv') }}" class="contact__export">
+                    エクスポート
+                </a>
+            </div>
+            <div class="admin__pagination">
+                {{$contacts->links()}}
+            </div>
+        </div>
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+            @endif
+            @if(session('redirect_success'))
+                <meta http-equiv="refresh" content="3;url={{ route('admin.index') }}">
+                @php
+                    session()->forget('redirect_success');
+                @endphp
+            @endif
+            <div class="listing__hail">
+                <table class="contact-table-inner" id="contact__table">
+                    <tr class="contact-table-row">
+                        <th class="contact-table-header">お名前</th>
+                        <th class="contact-table-header">性別</th>
+                        <th class="contact-table-header">メールアドレス</th>
+                        <th class="contact-table-header">お問い合わせの種類</th>
+                        <th class="contact-table-header">お問い合わせの内容</th>
+                        <th></th>
+                    </tr>
+                    @foreach($contacts as $contact)
+                    <tr class="contact-row">
+                        <td class="contact-cell" style="display:none;">
+                            {{ $contact->id }}
+                        </td>
+                        <td class="contact-cell">
+                            {{ $contact->first_name }} {{ $contact->last_name }}
+                        </td>
+                        <td class="contact-cell">
+                            @if ($contact->gender == 1)
+                                男性
+                            @elseif ($contact->gender == 2)
+                                女性
+                            @else
+                                その他
+                            @endif
+                        </td>
+                        <td class="contact-cell">
+                            {{ $contact->email }}
+                        </td>
+                        <td class="contact-cell">
+                            {{ $contact->category->content }}
+                        </td>
+                        <td class="contact-cell">
+                            <a href="#modal-{{ $contact->id }}" class="modal_admin" id="modal-link-{{ $contact->id }}">
+                                詳細
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
         @foreach($contacts as $contact)
         <div class="modal-wrapper" id="modal-{{ $contact->id }}">
             <a href="#!" class="modal-overlay"></a>
@@ -222,7 +214,7 @@
                     </table>
                 </div>
                 <div class="modal_delete"  id="modal-{{ $contact->id }}">
-                        <form action="{{ route('admin.destroy', ['id' =>$contact->id]) }}" method="POST">
+                    <form action="{{ route('admin.destroy', ['id' =>$contact->id]) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" onclick="return confirm('本当に削除しますか？')" class="delete-btn">削除</button>
@@ -230,10 +222,9 @@
                         <a href="#!" class="modal-close">
                             ×
                         </a>
-                    </div>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
     @endsection

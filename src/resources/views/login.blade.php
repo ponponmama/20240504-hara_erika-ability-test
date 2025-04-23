c08bc4380c1e10bef70f2e8fca5c963ae4072d5b
ponponmama <buti1024@gmail.com>
2025-04-23 21:33:03 +0900

    login余分なdiv削除＆css調整

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -19,38 +19,26 @@
     <form class="page-main-form" action="/login" method="post">
         @csrf
         <div class="form-group">
-            <div class="form-group-title">
-                <label class="form-label-item form-label--item" for="email">
-                    メールアドレス
-                </label>
-            </div>
-            <div class="form-group-content">
-                <div class="form-input-text">
-                    <input class="contact-item" type="email" id="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" >
-                </div>
-                <div class="form-error">
+            <label class="form-label-item form-label--item" for="email">
+                メールアドレス
+            </label>
+            <input class="contact-item" type="email" id="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" >
+            <p class="form-error">
                     @error('email')
                         {{ $message }}
                     @enderror
-                </div>
-            </div>
+            </p>
         </div>
         <div class="form-group">
-            <div class="form-group-title">
-                <label class="form-label-item" for="password">
-                    パスワード
-                </label>
-            </div>
-            <div class="form-group-content">
-                <div class="form-input-text">
-                    <input class="contact-item" type="password" id="password"  name="password" placeholder="coachtech1106" >
-                </div>
-                <div class="form-error">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-                </div>
-            </div>
+            <label class="form-label-item" for="password">
+                パスワード
+            </label>
+            <input class="contact-item" type="password" id="password"  name="password" placeholder="coachtech1106" >
+            <p class="form-error">
+                @error('password')
+                    {{ $message }}
+                @enderror
+            </p>
         </div>
         <button class="button login-btn login-button" type="submit">
             ログイン
